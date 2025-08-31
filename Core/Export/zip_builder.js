7c100255f038e80e9060a2ce4077abef00a416f6
45846ab <134137554+45846ab@users.noreply.github.com>
2025-09-01 06:09:11 +0900

    Add files via upload
    Signed-off-by: 45846ab <134137554+45846ab@users.noreply.github.com>

--- Core/Export/zip_builder.js
+++ Core/Export/zip_builder.js
@@ -1,1 +1,11 @@
+import fs from 'fs';
+import archiver from 'archiver';
 
+export function createZip(modelPath, outZipPath) {
+  const output = fs.createWriteStream(outZipPath);
+  const archive = archiver('zip', { zlib: { level: 9 } });
+
+  archive.pipe(output);
+  archive.file(modelPath, { name: 'monster.glb' });
+  archive.finalize();
+}
