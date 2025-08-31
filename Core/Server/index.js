9c0a40ffb17402c4f5c6743c3933db4b2762b3ee
45846ab <134137554+45846ab@users.noreply.github.com>
2025-09-01 06:09:58 +0900

    Add files via upload
    Signed-off-by: 45846ab <134137554+45846ab@users.noreply.github.com>

--- Core/Server/index.js
+++ Core/Server/index.js
@@ -1,1 +1,17 @@
+import express from "express";
+import { buildPrompt } from "../prompt/style_prompt_builder.js";
+import { callGemini } from "../generator/gemini_api_caller.js";
 
+const app = express();
+app.use(express.json());
+
+app.post("/generate", async (req, res) => {
+  const { name, description, style } = req.body;
+  const prompt = buildPrompt({ name, description, style });
+  const modelData = await callGemini(prompt);
+  res.json(modelData);
+});
+
+app.listen(3000, () => {
+  console.log("🧠 MonsterForge Core API listening on http://localhost:3000");
+});
